a290dfd81def152fbfa556646465d3d03904434f
knowsach <knowsach@gmail.com>
2020-06-28 22:46:44 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html"
+})
+export class AppComponent {
+  array = ["btn 1", "btn 2", "btn 3", "btn 4", "btn 5"];
+}
